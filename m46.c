fc6a5b1066d1294362bfedea4ab87de9522b71d3
manojeminem <35653185+manojeminem@users.noreply.github.com>
2018-03-02 14:34:55 +0530

    Create m46.c

---
+++ m46.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+char senten[30];
+int i,ct=1;
+scanf("%s",&senten);
+for(i=0;i<sentena[i]!=null;i++)
+{
+if(sentena[i]='@'||sentena[i]='#'||sentena[i]='$'||sentena[i]='&')
+{
+c++;
+}
+}
+printf("%d",ct);
+}
